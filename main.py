9a05b0116fdd6279b83baf3faa824e176f8cdc25
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-08-17 11:10:43 +0300

    Kütüphanelerin eklenmesi ve test.csv train.csv'in eklenmesi

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+import os
