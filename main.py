a554b83a6bf3a19a03b8267573214ce7ebd5f4b3
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-08-22 14:32:22 +0300

    LinearRegression modeli oluşturulup test edildi. NaN olan değerlerin olduğu tespit edilip bu satılar silindi.

--- main.py
+++ main.py
@@ -15,8 +15,20 @@ print(test_df.head())
 test_row, test_col = test_df.shape
 print(f"\nThere are {train_row} rows and {train_col} columns in test dataframe")
 
+# y sütununda NaN olan değerleri içeren satıları kaldırır.
+train_df.drop(train_df[train_df.y.isnull()].index, inplace=True)
+
 # .values.reshape(-1, 1), bu değerleri bir sütun vektörüne dönüştürür.
 X_train = train_df["x"].values.reshape(-1, 1)
 y_train = train_df["y"].values.reshape(-1, 1)
 x_test = train_df["x"].values.reshape(-1, 1)
 y_test = train_df["y"].values.reshape(-1, 1)
+
+# sklearn kütüphenesinden model örneği oluşturulması.
+reg = LinearRegression()
+# Modelin veri setindeki ilişkinin öğrenilmesi sağlanır
+reg.fit(X_train, y_train)
+# x_test verisini modele verip, modelin tahmin ettiği y değerleri elde edilir
+y_predictions = reg.predict(x_test)
+mse = mean_squared_error(y_test, y_predictions)
+print(f"The result of MSE is: {mse}")
